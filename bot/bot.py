6eb67af9997bf13a903bd7e63b07073e50549938
justRunnz <ruigaspar03@hotmail.com>
2021-12-16 21:03:36 +0100

    add loop status

--- bot/bot.py
+++ bot/bot.py
@@ -1,25 +1,31 @@
+import asyncio
+from itertools import cycle
 from pathlib import Path
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 class MusicBot(commands.Bot):
+
     def __init__(self):
         self._cogs = [p.stem for p in Path(".").glob("./bot/cogs/*.py")]
+        self.status = cycle(['made by Runnz', 'bot.py', '.help'])
+        print("cog loaded")
         super().__init__(command_prefix=self.prefix, case_insensitive=True, intents = discord.Intents.all())
 
     def setup(self):
+        self._cogs = [p.stem for p in Path(".").glob("./bot/cogs/*.py")]
         print("Running setup ...")
 
         for cog in self._cogs:
             self.load_extension(f'bot.cogs.{cog}')
             print(f"Loaded `{cog}` cog.")
-        
+
         print("Setup complete!")
 
     def run(self):
         self.setup()
-        with open("data/token.0", "r", encoding="utf-8") as f:
+        with open("data/token", "r", encoding="utf-8") as f:
             TOKEN = f.read()
 
         print("Running bot!")
@@ -47,10 +53,18 @@ class MusicBot(commands.Bot):
 
     async def on_command_error(self, ctx, exc):
         raise getattr(exc,"original", exc)
- 
+
+    @commands.Cog.listener()
     async def on_ready(self):
         self.client_id = (await self.application_info()).id
+        await self.wait_until_ready()
+        self.change_status.start()
         print("Bot ready to go...")
+
+    @tasks.loop(seconds=3.0)
+    async def change_status(self):
+        await self.change_presence(activity=discord.Game(next(self.status)))
+
 
     async def prefix(self,bot,msg):
         return commands.when_mentioned_or(".")(bot,msg)
