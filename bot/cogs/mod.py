58649dee05ff754db1ac943d20c64d8c325364fe
justRunnz <ruigaspar03@hotmail.com>
2021-12-16 21:05:32 +0100

    add mod.py(add clear command)

---
+++ bot/cogs/mod.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from discord import Embed, Member, NotFound, Object
+from discord.ext.commands import Cog, Greedy, Converter
+from discord.ext.commands import command, has_permissions, bot_has_permissions
+from discord.ext import commands
+
+
+class Mod(commands.Cog):
+    @command(name="clear", aliases=["purge","clr","cl"])
+    @bot_has_permissions(manage_messages=True)
+    @has_permissions(manage_messages=True)
+    async def clear_messages(self, ctx, targets: Greedy[Member], limit: Optional[int] = 1):
+        def _check(message):
+            return not len(targets) or message.author in targets
+
+        if 0 < limit <= 100:
+            with ctx.channel.typing():
+                await ctx.message.delete()
+                deleted = await ctx.channel.purge(limit=limit, after=datetime.utcnow() - timedelta(days=1000),
+                                                  check=_check)
+
+                await ctx.send(f"Deleted {len(deleted):,} messages.", delete_after=5)
+
+        else:
+            await ctx.send("The limit provided is not within acceptable bounds.")
+
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
